9319c1ccef088741f60b8fc49fc5b97aa34e5899
Baranova <syniovych.tanya@gmail.com>
2024-10-11 00:31:09 +0300

    task-home

--- js/homework.js
+++ js/homework.js
@@ -237,10 +237,98 @@
 // console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]))
 // console.log(getCommonElements([1, 2, 3], [10, 20, 30]))
 // console.log(getCommonElements())
+//////////////////
+
+
+// function calculateTotalPrice(order) {
+//   let sum = 0;
+//   for (let calculateTotal of order) {
+//     sum += calculateTotal;
+//   }
+//   return sum;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]))
+// console.log(calculateTotalPrice([164, 48, 291]))
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]))
+// console.log(calculateTotalPrice([]))
+// console.log(calculateTotalPrice())
+///////////
+
+
+// function createReversedArray() {
+//   const args = Array.from(arguments);
+//   return args.toReversed();
+// }
+
+// console.log(createReversedArray(12, 85, 37, 4))
+// console.log(createReversedArray(164, 48, 291))
+// console.log(createReversedArray(412, 371, 94, 63, 176))
+// console.log(createReversedArray())
+/////////////////
+// for (i = array; i < maxLength; i++){
+
+//
 
 
 
-// console.log()
+// Таск 1 дз"3"//
+
+function slugify(title) {
+    let slug = title.split(" ").join("-");
+    return slug.toLowerCase();
+}
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+////////////////////
+
+
+// Таск 2 дз"3"//
+
+function makeArray(firstArray, secondArray, maxLength) {
+    const array = firstArray.concat(secondArray);
+    
+    if (array.length > maxLength) {
+        return array.slice(0, maxLength);
+    }
+    else {
+        return array;
+    }
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
+////////////////
+
+
+// Таск 3 дз"3"//
+
+function filterArray(numbers, value) {
+    const terArray = [];
+    for (const i of numbers) {
+        if (i > value) {
+            terArray.push(i);
+        }
+    }
+    return terArray;
+ }
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
 // console.log()
 // console.log()
 // console.log()
