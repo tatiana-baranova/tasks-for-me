2d0207db4986da730066f373750cd1312dbac25e
Tetiana Baranova <syniovych.tania@gmail.com>
2024-11-13 00:33:29 +0200

    color-task

--- js/blok-6.js
+++ js/blok-6.js
@@ -475,3 +475,50 @@
 
 // yura.email = "superyura@gmail.com";
 // console.log("yura", yura.email);
+
+
+///приклад створення палітри кольорів///
+
+// const colorPalette = document.querySelector(".color-palette");
+// const output = document.querySelector(".output");
+
+// colorPalette.addEventListener("click", selectColor);
+
+// // This is where delegation «magic» happens
+// function selectColor(event) {
+//   if (event.target.nodeName !== "BUTTON") {
+//     return;
+//   }
+
+//   const selectedColor = event.target.dataset.color;
+//   output.textContent = `Selected color: ${selectedColor}`;
+//   output.style.color = selectedColor;
+// }
+
+// // Some helper functions to render palette items
+// createPaletteItems();
+
+// function createPaletteItems() {
+//   const items = [];
+//   for (let i = 0; i < 60; i++) {
+//     const color = getRandomHexColor();
+//     const item = document.createElement("button");
+//     item.type = "button";
+//     item.dataset.color = color;
+//     item.style.backgroundColor = color;
+//     item.classList.add("item");
+//     items.push(item);
+//   }
+//   colorPalette.append(...items);
+// }
+
+// function getRandomHexColor() {
+//   const letters = "0123456789ABCDEF";
+//   let color = "#";
+
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+
+//   return color;
+// }
